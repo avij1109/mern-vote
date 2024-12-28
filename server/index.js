0337b62f58cb6eb5c472cb3a7c8fa6e4668a880d
avij1109 <avinashjain768@gmail.com>
2024-12-29 01:57:52 +0530

    xd

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const handle = require('./handlers');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db=require('./models')
+
+
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT;
 
 app.get('/',(req,res) => res.json({hello:'world'}));
 
 app.use(handle.notFound);
+
 app.use(handle.errors)
 app.listen(port,console.log('server started on 4000'));
 
